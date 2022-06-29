b2175848c2d65ccbcab0ac1365e10f3878bcf19c
SampleUserD <61961965+SampleUserD@users.noreply.github.com>
2022-06-29 20:59:01 +0500

    Update package.json + entry point

---
+++ Main.ts
@@ -0,0 +1,9 @@
+import * as Commons from '#commons'
+import * as Scanner from '#scanner'
+import * as Parser from '#parser'
+
+export {
+  Commons as Commons,
+  Scanner as Scanner,
+  Parser as Parser
+}
